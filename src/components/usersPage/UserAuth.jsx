2ada56ab4f093d93e91adac25dbffc0b50cf9375
Bayz.ng <ayofe70@gmail.com>
2024-02-24 15:32:59 -0800

    fifth-commit

--- src/components/usersPage/UserAuth.jsx
+++ src/components/usersPage/UserAuth.jsx
@@ -6,18 +6,18 @@ import { AuthContext } from '../context/AuthContext'
 const UserAuth = () => {
   const {currentUser} = useContext(AuthContext)
   return (
-    <>
+    <div id='userHeader'>
       <div>
         <div className='usersAuth'>
           <img style={{width: '6rem', height: "5.7rem", borderRadius: '40px'}} src={currentUser.photoURL} alt="" />
-          <p><strong>Welcome back,</strong> <strong style={{textTransform: "uppercase"}}>{currentUser.displayName}</strong></p> <br />
+          <h4>Welcome back, <strong style={{textTransform: "uppercase"}}>{currentUser.displayName}</strong></h4> <br />
         </div>
         <p className='usersP'>Send crypto with ease.</p>
       </div>
       <div>
         <ChangeNetwork />
       </div>
-    </>
+    </div>
   );
 };
 
